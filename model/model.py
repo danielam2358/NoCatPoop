d490c88443975db3dfec43db94ad3064160eb431
danielam <danielamram953@google.com>
2023-01-18 22:26:29 +0200

    Decouple config files for model

--- model/model.py
+++ model/model.py
@@ -3,16 +3,15 @@ from datetime import datetime
 import cv2 as cv
 import numpy as np
 
-CLASSES = open('yolo/coco.names').read().strip().split('\n')
-COLORS = np.random.randint(0, 255, size=(len(CLASSES), 3), dtype='uint8')
-
 Detection = namedtuple("Detection", ["box", "confidence", "class_id", "class_name"])
 
 
 class Model:
 
-    def __init__(self):
-        self._net = cv.dnn.readNetFromDarknet('yolo/yolov3.cfg', 'yolo/yolov3.weights')
+    def __init__(self, config_file_path, weights_file_path, classes_file_path):
+        self._net = cv.dnn.readNetFromDarknet(config_file_path, weights_file_path)
+        self._classes = open(classes_file_path).read().strip().split('\n')
+        self._colors = np.random.randint(0, 255, size=(len(self._classes), 3), dtype='uint8')
         self._net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
 
         ln = self._net.getLayerNames()
@@ -31,8 +30,7 @@ class Model:
         outputs = np.vstack(outputs)
         return outputs
 
-    @staticmethod
-    def process_outputs(image: np.ndarray, outputs: np.ndarray, conf: float) -> list[Detection]:
+    def process_outputs(self, image: np.ndarray, outputs: np.ndarray, conf: float) -> list[Detection]:
         image_height, image_weight = image.shape[:2]
 
         detections: list[Detection] = []
@@ -58,26 +56,25 @@ class Model:
             for i in indices.flatten():
                 (x, y) = (boxes[i][0], boxes[i][1])
                 (w, h) = (boxes[i][2], boxes[i][3])
-                color = [int(c) for c in COLORS[classIDs[i]]]
+                color = [int(c) for c in self._colors[classIDs[i]]]
                 cv.rectangle(image, (x, y), (x + w, y + h), color, 2)
                 detection = Detection(
                     box=boxes[i],
                     confidence=confidences[i],
                     class_id=class_ids[i],
-                    class_name=CLASSES[class_ids[i]]
+                    class_name=self._classes[class_ids[i]]
                 )
                 detections.append(detection)
 
         return detections
 
-    @staticmethod
-    def draw_detections(image: np.ndarray, detections: list[Detection]) -> np.ndarray:
+    def draw_detections(self, image: np.ndarray, detections: list[Detection]) -> np.ndarray:
         image = image.copy()
 
         for detection in detections:
             (x, y) = (detection.box[0], detection.box[1])
             (w, h) = (detection.box[2], detection.box[3])
-            color = [int(c) for c in COLORS[detection.class_id]]
+            color = [int(c) for c in self._colors[detection.class_id]]
             cv.rectangle(image, (x, y), (x + w, y + h), color, 2)
             text = "{}: {:.4f}".format(detection.class_name, detection.confidences)
             cv.putText(image, text, (x, y - 5), cv.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
